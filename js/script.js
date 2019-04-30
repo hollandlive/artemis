7d7ffcbaf28ab79bf47ba6727036f9f1f1e56593
Artem Aksenov <hollandlive@gmail.com>
2019-04-30 10:39:57 +0200

    css

--- js/script.js
+++ js/script.js
@@ -6,4 +6,19 @@ $(document).ready(function(){
   $('.btn-more-toggle').click(function(){
     $('#about').toggleClass('scrolled')
   })
+
+  // $('.navbar a').on('click', function(e){
+  //   if(this.hash !== '') {
+  //     e.preventDefault();
+  //
+  //     const hash = this.hash;
+  //
+  //     $('html, body').animate({
+  //       {
+  //         scrollTop: $(hash).offset().top
+  //       },
+  //       800
+  //     });
+  //   }
+  // });
 })
