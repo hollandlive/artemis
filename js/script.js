4b8df6437653b8b2b27ce77f9e6084f789d1de2c
Artem Aksenov <hollandlive@gmail.com>
2019-04-30 19:10:14 +0200

    js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
-// $(document).ready(function(){
    $('.menu-toggle').click(function(){
      $('nav').toggleClass('active')
   })
+
+
+
+
+
+
 //
 //   $('.btn-more-toggle').click(function(){
 //     $('#about').toggleClass('scrolled')
